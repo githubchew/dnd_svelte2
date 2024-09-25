8f2f68e0c4dd347ac54e02e9da17c0d6677ecde4
Hubchew <iammusicchu@gmail.com>
2024-09-25 23:20:45 +0800

    changed about to dnd page

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -19,8 +19,8 @@
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
 				<a href="/">Home</a>
 			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
+			<li aria-current={$page.url.pathname === '/dnd' ? 'page' : undefined}>
+				<a href="/dnd">dnd</a>
 			</li>
 			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
 				<a href="/sverdle">Sverdle</a>
