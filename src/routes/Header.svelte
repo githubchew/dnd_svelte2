bb48ccaa4073ee3c719b3e12f53f8b3a8e91dbdb
Hubchew <iammusicchu@gmail.com>
2024-10-18 16:30:20 +0800

    add fc to header.

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -47,6 +47,9 @@
 			<li aria-current={$page.url.pathname === '/swt2' ? 'page' : undefined}>
 				<a href="/swt2">swt2</a>
 			</li>
+			<li aria-current={$page.url.pathname === '/flashcards' ? 'page' : undefined}>
+				<a href="/flashcards">fc</a>
+			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
